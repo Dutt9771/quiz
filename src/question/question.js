6e93070415f79eeaf672e747539e98565585cae3
Dutt Mistry <dutta.mistry@gmail.com>
2023-04-19 20:51:44 +0530

    Done Quiz

--- src/question/question.js
+++ src/question/question.js
@@ -1,92 +1,159 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+export default function Questions() {
+  let questions = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "Madrid", "Rome", "Berlin"],
+      answer: "Paris",
+    },
+    {
+      question: "What is the largest planet in the solar system?",
+      options: ["Jupiter", "Saturn", "Uranus", "Neptune"],
+      answer: "Jupiter",
+    },
+    {
+      question: "What is the highest mountain in the world?",
+      options: ["Everest", "Kilimanjaro", "Denali", "Aconcagua"],
+      answer: "Everest",
+    },
+    {
+      question: "Who wrote the Harry Potter series of books?",
+      options: [
+        "J.K. Rowling",
+        "Stephen King",
+        "Dan Brown",
+        "George R.R. Martin",
+      ],
+      answer: "J.K. Rowling",
+    },
+  ];
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [open, setOpen] = useState(true);
 
-
-export default function Questions(){
-  let questions=  [ {
-        question: 'What is the capital of France?',
-        options: ['Paris', 'Madrid', 'Rome', 'Berlin'],
-        answer: 'Paris'
-      },
-      {
-        question: 'What is the largest planet in the solar system?',
-        options: ['Jupiter', 'Saturn', 'Uranus', 'Neptune'],
-        answer: 'Jupiter'
-      },
-      {
-        question: 'What is the highest mountain in the world?',
-        options: ['Everest', 'Kilimanjaro', 'Denali', 'Aconcagua'],
-        answer: 'Everest'
-      },
-      {
-        question: 'Who wrote the Harry Potter series of books?',
-        options: ['J.K. Rowling', 'Stephen King', 'Dan Brown', 'George R.R. Martin'],
-        answer: 'J.K. Rowling'
-      }
-  ]
-  let  [question,setQuestions]=useState(questions[0])
-  const [currentQuestion,setCurrentQuestion]=useState(1)
-  const [score,setScore]=useState(1)
-  const [open, setOpen] = useState(false);
-  
-  
-//   question=questions[0]
-
-let btn="Next Question"
-  let answer
-  const answerCheck=(e)=>{
-    answer=e.target.value
-    console.log("Answer",answer)
-    console.log("questions[currentQuestion].answer",questions[currentQuestion-1].answer)
-    
-}
-const nextQuestion=()=>{
-    if(answer===questions[currentQuestion-1].answer){
-        setScore(score+1)
-        console.log("questions[currentQuestion].answer",questions[currentQuestion-1].answer)
-        console.log("scrore",score)
-        if((currentQuestion-1)===questions.length){
-            btn="Finish"
-    }else{
-        
-        setCurrentQuestion(currentQuestion+1)
-        setQuestions(questions[currentQuestion])
-        console.log("question",question)
-    }}else{
-        if((currentQuestion-1)===questions.length){
-            console.log("(currentQuestion-1)",(currentQuestion-1))
-            setOpen(false)
-            btn="Finish"
-    }else{
-        
-        setCurrentQuestion(currentQuestion+1)
-        setQuestions(questions[currentQuestion])
-        console.log("question",question)
+  //   question=questions[0]
+  const answerQuestion = (answer) => {
+    if (answer === questions[currentQuestion].answer) {
+      setScore(score + 1);
     }
+    const nextQue = currentQuestion + 1;
+    console.log("currentQuestion", currentQuestion + 1);
+    if (nextQue === questions.length) {
+      setOpen(false);
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+      console.log("currentQuestion", currentQuestion);
     }
-      
-}
-    return (
-<div>
+  };
 
-
+  const resetQuiz = () => {
+    setCurrentQuestion(0);
+    setScore(0);
+    setOpen(true);
+  };
+  return (
     <div>
-        {open?(
-<>
-            <h1>{question.question}</h1>
-            {question.options.map((option,index)=>{
-                return(
-                    <div>
+      <div sx={{ 
+      float:'right'}}>
+      <Button
+        variant="outlined"
+        type="button"
+        onClick={resetQuiz}
+        sx={{
+          
+      float:'right',
+          marginTop: "5px",
+          marginBottom: "5px",
+          marginRight: "8px",
+          color: "black",
+          border: "1px solid black",
+          "&:hover": {
+            color: "white",
+            backgroundColor: "green",
+            border: "1px solid green",
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      >
+        RESET
+      </Button>
 
-            <label htmlFor="{option}">{option}</label>
-            <input id='{option}' type='radio' name='questions' value={option} onChange={answerCheck}/>
-            </div>
-        )
-    })}
-    <button type='button' id='btn' onClick={nextQuestion}>{btn}</button>
-    </>
-    ):null}
+          </div>
+      <div style={{marginTop:"30px"}}>
+        {open ? (
+          <>
+            <h1>{questions[currentQuestion].question}</h1>
+            {questions[currentQuestion].options.map((option, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      width: "700px",
+                      backgroundColor: "white",
+                      "&:hover": {
+                        color: "green",
+
+                        opacity: [0.9, 0.8, 0.7],
+                      },
+                      cursor: "pointer",
+                    }}
+                  >
+                    <Stack
+                      direction="column"
+                      justifyContent="center"
+                      alignItems="center"
+                      spacing={2}
+                      onClick={() => answerQuestion(option)}
+                      type="button"
+                      style={{
+                        display: "container",
+                        padding: "25px",
+                        borderRadius: "5px",
+                        margin: "10px",
+                        marginRight: "10px",
+                        justifyContent: "center",
+                      }}
+                      sx={{
+                        color: "black",
+                        border: "1px solid black",
+                        "&:hover": {
+                          color: "white",
+                          backgroundColor: "green",
+                          border: "1px solid green",
+                          opacity: [0.9, 0.8, 0.7],
+                        },
+                      }}
+                    >
+                      {option}
+                    </Stack>
+                  </Box>
+                </div>
+              );
+            })}
+            {/* <button type='button' id='btn' onClick={nextQuestion}>{btn}</button> */}
+          </>
+        ) : (
+          <div>
+            <h4>Quiz Result</h4>
+            <h1>
+              {score}/{questions.length}
+            </h1>
+            <h1 style={{color:"blue"}}>
+            "To retake the quiz, Click on the 'Reset' button.
+            </h1>
+          </div>
+        )}
+      </div>
+      {/* <div >Scrore is {score}</div> */}
     </div>
-{/* <div >Scrore is {score}</div> */}
-</div>
-    );
+  );
 }
